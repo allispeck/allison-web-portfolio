4c65b63d436d31a7d52b12a447ea78245b97f4f0
allison <allisonmspeck@gmail.com>
2020-01-25 17:26:52 -0500

    Contact Form
    testing contact form

---
+++ app.js
@@ -0,0 +1,39 @@
+
+
+//form js
+
+$(document).ready(function(){
+    $('.flip').click(function(){
+        $('.cont-flip').toggleClass('flipped');
+        return false;
+    });
+
+    $('.contact-form').submit(function (e) {
+        e.preventDefault();
+        $('.cont-flip').toggleClass('flipped');
+
+        $('#submitBtn').text('Thank you!');
+
+        setTimeout(location.reload.bind(location), 2000);
+    })
+
+});
+
+// smooth scroll
+
+$(document).ready(function () {
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+
+$('.carousel').carousel();
