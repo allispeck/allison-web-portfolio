8a1a2f023ca99631aafa25348e0e0486ae139e65
allison <allisonmspeck@gmail.com>
2020-01-25 18:29:57 -0500

    toasty

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 //form js
 
 $(document).ready(function () {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        onOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    });
+    const url = new URL(window.location.href);
+
+    if (url.searchParams.get('email-sent')) {
+        Toast.fire({
+            icon: 'success',
+            title: 'Message Sent!'
+        });
+    }
+
+
     $('.flip').click(function () {
         $('.cont-flip').toggleClass('flipped');
         return false;
@@ -34,4 +55,3 @@ $(document).ready(function () {
 });
 
 
-$('.carousel').carousel();
