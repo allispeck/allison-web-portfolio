c4280cb5c15d925b0383b1546764ddcd780cc3da
allison <allisonmspeck@gmail.com>
2021-01-23 17:17:20 -0500

    navFix

--- build.js
+++ build.js
@@ -160,6 +160,45 @@ $(document).ready(function () {
       fontColor: "white"
     });
   }
+}); // navbar collapse
+
+$(document).ready(function () {
+  "use strict";
+
+  var myNav = {
+    init: function init() {
+      this.cacheDOM();
+      this.browserWidth();
+      this.bindEvents();
+    },
+    cacheDOM: function cacheDOM() {
+      this.navBars = $(".navBars");
+      this.xBxHack = $("#xBxHack");
+      this.navMenu = $("#menu");
+    },
+    browserWidth: function browserWidth() {
+      $(window).resize(this.bindEvents.bind(this));
+    },
+    bindEvents: function bindEvents() {
+      var width = window.innerWidth;
+
+      if (width < 600) {
+        this.navBars.click(this.animate.bind(this));
+        this.navMenu.hide();
+        this.xBxHack[0].checked = false;
+      } else {
+        this.resetNav();
+      }
+    },
+    animate: function animate(e) {
+      var checkbox = this.xBxHack[0];
+      !checkbox.checked ? this.navMenu.slideDown() : this.navMenu.slideUp();
+    },
+    resetNav: function resetNav() {
+      this.navMenu.show();
+    }
+  };
+  myNav.init();
 });
 
 /***/ }),
